4912bbd6f1975cad9ecaa625e9491cffdb853157
andyr <forpetessake@protonmail.com>
2017-10-30 13:38:24 +0000

    Add files via upload

---
+++ chap3/min.js
@@ -0,0 +1,16 @@
+function min(a,b) {
+	if (a < b) return a
+	else return b
+}
+console.log(min(3,4))
+console.log(min(5,4))
+console.log(min(-1,-3))
+console.log(min(-3,-1))
+console.log(min(-10,10))
+console.log(min(10,-10))
+console.log(min(0,10))
+console.log(min(10,0))
+
+
+
+
